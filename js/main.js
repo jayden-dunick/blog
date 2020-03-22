12ba469179e70041bb8e9a3645a2a05136f3de6c
jayden-dunick <62521054+jayden-dunick@users.noreply.github.com>
2020-03-23 12:14:31 +1300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+mybutton = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} 
